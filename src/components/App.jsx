9d0836e4abb2ebcddd5b8e353bfe46902dc345f8
Yuriy <82809592+yuriy-o@users.noreply.github.com>
2023-02-05 22:46:36 +0200

    rewriting with React Hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,4 @@
-import React, { Component } from 'react';
-// import { nanoid } from 'nanoid';
-// import PropTypes from 'prop-types';
+import { useState, useEffect } from 'react';
 
 import { ContactForm } from './ContactForm/ContactForm';
 import { ContactList } from './ContactList/ContactList';
@@ -9,69 +7,46 @@ import { nanoidUA } from './additions/nanoidUA';
 
 import { Container, H1, H2, Warning } from './App.styled';
 
-export class App extends Component {
-  state = {
-    contacts: [],
-    filter: '',
-  };
+const LOCAL_STORAGE = JSON.parse(window.localStorage.getItem('contacts'));
 
-  componentDidMount() {
-    const savedContacts = localStorage.getItem('contacts');
-    const parseContacts = JSON.parse(savedContacts);
+export const App = ({ initialContacts }) => {
+  const [contacts, setContacts] = useState(LOCAL_STORAGE ?? initialContacts);
+  const [filter, setFilter] = useState('');
 
-    if (savedContacts === null) {
-      return this.setState({ contacts: this.props.initialContacts });
-    }
-    if (parseContacts) {
-      this.setState({ contacts: parseContacts });
-    }
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.contacts !== prevState.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
 
   //! (addContact)
-  formHandleSubmit = (values, { resetForm }) => {
+  const formHandleSubmit = (values, { resetForm }) => {
     resetForm();
 
     const { name, number } = values;
     const contact = { name, number };
 
-    const dublicateContact = this.findDublicateContact(
-      contact,
-      this.state.contacts
-    );
+    const dublicateContact = findDublicateContact(contact, contacts);
 
     dublicateContact
       ? alert(`${contact.name} is already in contacts`)
-      : this.setState(prevState => ({
-          contacts: [...prevState.contacts, { ...values, id: nanoidUA() }],
-        }));
+      : setContacts([...contacts, { ...values, id: nanoidUA() }]);
   };
 
   //! Функція перевірки імені перед додаванням з урахуванням регистру
-  findDublicateContact = (contact, contactsList) => {
+  const findDublicateContact = (contact, contactsList) => {
     return contactsList.find(
       item => item.name.toLowerCase() === contact.name.toLowerCase()
     );
   };
 
-  removeContact = id => {
-    this.setState(({ contacts }) => {
-      const newContacts = contacts.filter(contact => contact.id !== id);
-      return { contacts: newContacts };
-    });
+  const removeContact = id => {
+    setContacts(contacts.filter(contact => contact.id !== id));
   };
-  handleInputChange = e => {
-    const { name, value } = e.target;
-    this.setState({ [name]: value });
-  };
-  filterContactByName = () => {
-    const { filter, contacts } = this.state;
 
+  const handleInputChange = e => {
+    setFilter(e.target.value);
+  };
+
+  const filterContactByName = () => {
     //! якщо фільтр пустий, то відразу показуємо контакти
     if (!filter) {
       return contacts;
@@ -83,23 +58,23 @@ export class App extends Component {
     });
   };
 
-  render() {
-    const { removeContact, handleInputChange, formHandleSubmit } = this;
-    const contacts = this.filterContactByName();
-    const isContacts = Boolean(contacts.length);
+  const isContacts = Boolean(contacts.length);
 
-    return (
-      <Container>
-        <H1>Phonebook</H1>
-        <ContactForm onSubmitForm={formHandleSubmit} />
-        <H2>Contacts</H2>
-        <ContactsFilter handleInputChange={handleInputChange} />
+  return (
+    <Container>
+      <H1>Phonebook</H1>
+      <ContactForm onSubmitForm={formHandleSubmit} />
 
-        {isContacts && (
-          <ContactList removeContact={removeContact} contacts={contacts} />
-        )}
-        {!isContacts && <Warning>No contacts in the list</Warning>}
-      </Container>
-    );
-  }
-}
+      <H2>Contacts</H2>
+      <ContactsFilter handleInputChange={handleInputChange} />
+
+      {isContacts && (
+        <ContactList
+          removeContact={removeContact}
+          contacts={filterContactByName()}
+        />
+      )}
+      {!isContacts && <Warning>No contacts in the list</Warning>}
+    </Container>
+  );
+};
